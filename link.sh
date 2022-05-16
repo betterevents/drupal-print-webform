b5eb6878ff4e4b332f84ae8e96909e630262d0d2
Stavros Kounis <skounis@gmail.com>
2022-05-16 20:55:38 +0200

    chore(config) Delete before symlink.

--- link.sh
+++ link.sh
@@ -1,1 +1,3 @@
-ln -sfr ./config/sync ./web/sites/default/files/sync
+rm ./web/sites/default/files/sync -R
+ln -sfr ./config/sync ./web/sites/default/files/
+#ln -sfrT ./config/sync ./web/sites/default/files/sync
