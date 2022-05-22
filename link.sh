0bc8655e039daf1d859401101385d91ae0dffa2a
Stavros Kounis <skounis@gmail.com>
2022-05-22 20:18:39 +0200

    feat(install) Symlink custom

--- link.sh
+++ link.sh
@@ -1,3 +1,6 @@
 rm ./web/sites/default/files/sync -R
 ln -sfr ./config/sync ./web/sites/default/files/
+rm ./web/modules/custom
+ln -srf ./lib/modules ./web/modules/custom
+
 #ln -sfrT ./config/sync ./web/sites/default/files/sync
